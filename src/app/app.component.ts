d769f3d1dae2498d84676ac9ef9f30f9bb50c98e
Luna02020 <yuudai02021998taie@gmail.com>
2018-07-04 21:57:00 +0900

    チュートリアル

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+
+  wordchange() {
+    this.title = '(^^)/';
+  }
+}
+
+export class Hero {
+  id: number;
+  name: string;
+}
